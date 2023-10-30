bff644b2fbc0b84cbdf63b55e119d997efc63bc6
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-10-29 20:30:22 -0500

    added board

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var watchRouter = require('./routes/Watch');
+var gridbuildRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Watch', watchRouter);
+app.use('/board', gridbuildRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
