ff46845191178a8af5f4721e077e8ae16aab7814
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-10-29 19:59:50 -0500

    added three objects

--- routes/Watch.js
+++ routes/Watch.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results Watch' });
+  res.render('Watch', { title: 'Search Results Watch' });
 });
 
 module.exports = router;
